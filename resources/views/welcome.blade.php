93f80c93cdc85bc2add518a6418cc882be2fe170
Maxim <devmaxpro@gmail.com>
2018-04-17 01:06:40 +0300

    Some changes in gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com">Laravel</a>
+                    <a href="https://reactjs.org">ReactJS</a>
+                    <a href="https://github.com/maximenkodev/Messenger">GitHub</a>
                 </div>
             </div>
         </div>
